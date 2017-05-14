f245b44456745d5226218f5daf9039b496f4596f
Tom Streller <tom.streller@functional-games.de>
2017-05-14 13:35:30 +0200

    Add mul point

--- src/theorem/math/point.rs
+++ src/theorem/math/point.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Sub};
+use std::ops::{Add, Sub, Mul};
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Point {
@@ -18,6 +18,40 @@ impl Sub for Point {
     type Output = Point;
 
     fn sub(self, other: Point) -> Point {
-        Point { x: self.x + other.x, y: self.y + other.y }
+        Point { x: self.x - other.x, y: self.y - other.y }
+    }
+}
+
+impl Mul<i32> for Point {
+    type Output = Point;
+
+    fn mul(self, s: i32) -> Point {
+        Point { x: self.x * s, y: self.y * s }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use theorem::math::{Point};
+
+    #[test]
+    fn add_point() {
+        let v1 = Point { x: 1, y: 1 };
+        let v2 = Point { x: 2, y: 2 };
+        assert_eq!(v1 + v1, v2);
+    }
+
+    #[test]
+    fn sub_point() {
+        let v1 = Point { x: 1, y: 1 };
+        let v2 = Point { x: 0, y: 0 };
+        assert_eq!(v1 - v1, v2);
+    }
+
+    #[test]
+    fn mul_point() {
+        let v1 = Point { x: 1, y: 1 };
+        let v2 = Point { x: 2, y: 2 };
+        assert_eq!(v1 * 2, v2);
     }
 }
